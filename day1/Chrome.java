1f8d98c49fe3a140f53cc667b4d964a459a64a75
Indhuanusha <anushathirupathi23@gmail.com>
2025-02-01 16:21:10 +0530

    Add files via upload
    My assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	
+public static void main(String[] args) {
+	float rate=91.0f;
+	String WebsiteName="Google";
+	Boolean HadFood=false;
+	char initial='C';
+	int ReleaseYear=2008;
+	System.out.println(rate);
+	System.out.println(WebsiteName);
+	System.out.println(HadFood);
+	System.out.println(initial);
+	System.out.println(ReleaseYear);
+}
+
+}
